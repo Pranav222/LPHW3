aeeb157592a234a510be6f9230820b489f5b8acc
pranav <pranavkv222@gmail.com>
2017-09-08 09:40:57 +0000

    String formatting using .format() function

---
+++ ex6.py
@@ -0,0 +1,9 @@
+# string formatting using .format() functions
+
+name      = input (" Enter ur name : ")
+place     = input ("Enter ur place name : ")
+age       = input(" Enter ur age : ")
+collage   = input ("Enter ur collage name = ")
+workplace = input ("Enter ur working are = ")
+
+print("Hello iam {}. Iam coming from {}. Iam {} yers old. I have complted my studies from {}. Now iam working in {}" .format(name,place,age,collage,workplace)) 
